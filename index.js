cdb02fc73c07353b685d9f0947e19e88766d49a6
Julio <juliodavidchopite@gmail.com>
2020-02-03 16:59:53 -0300

    added an allBooks query that returns the details of all books

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const { ApolloServer, gql } = require('apollo-server')
 const { authors, books } = require('./temporalDatabase')
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String]
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
